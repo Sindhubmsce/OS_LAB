5e42ed02d9229cb2d82a00e30b2b5fccf0fcb6c6
Sindhubmsce <sindhurk.cs25@bmsce.ac.in>
2026-03-02 12:38:28 +0530

    Add files via upload

---
+++ fcfs.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int at[n], bt[n], ct[n], tat[n], wt[n], rt[n];
+    float total_tat = 0, total_wt = 0;
+
+    for(i = 0; i < n; i++) {
+        printf("\nProcess P%d\n", i+1);
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Burst Time: ");
+        scanf("%d", &bt[i]);
+    }
+
+    int time = 0;
+    for(i = 0; i < n; i++) {
+
+
+        if(time < at[i]) {
+            time = at[i];
+        }
+
+        rt[i] = time - at[i];
+        time += bt[i];
+        ct[i] = time;
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+
+        total_tat += tat[i];
+        total_wt += wt[i];
+    }
+
+
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+               i+1, at[i], bt[i], ct[i], tat[i], wt[i], rt[i]);
+    }
+
+    printf("\nAverage Turnaround Time = %.2f", total_tat/n);
+    printf("\nAverage Waiting Time = %.2f\n", total_wt/n);
+
+    return 0;
+}
+
